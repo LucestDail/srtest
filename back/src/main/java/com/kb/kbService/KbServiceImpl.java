85a9a82b72b091ad94e4103288c988ca9d57fae6
hyun <dhtmdgussksm@naver.com>
2022-08-14 18:28:59 +0900

    Update KbServiceImpl.java
    jsoup 이슈(https://github.com/jhy/jsoup/issues/1218)로 인한 url 호출방식 변경

--- back/src/main/java/com/kb/kbService/KbServiceImpl.java
+++ back/src/main/java/com/kb/kbService/KbServiceImpl.java
@@ -3,6 +3,7 @@ package com.kb.kbService;
 import java.net.URI;
 
 import org.jsoup.Jsoup;
+import org.jsoup.Connection.Response;
 import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -14,7 +15,8 @@ public class KbServiceImpl implements KbService {
 
     @Override
     public String getExchangeRate() throws Exception {
-        Document doc = Jsoup.connect("https://obank.kbstar.com/quics?page=C101423").get();
+        Response execute = Jsoup.connect("https://obank.kbstar.com/quics?page=C101423").execute();
+        Document doc = Jsoup.parse(execute.body());
         return doc.getElementById("inqueryTable").html();
     }
 
